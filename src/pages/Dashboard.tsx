987a7d39672f46d6ffd2161e56745303de736010
dedayorr <dedayo16@gmail.com>
2023-11-01 20:53:29 +0100

    deleteButton

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,15 +6,21 @@ import { BsSearch } from "react-icons/bs";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import axios from "axios";
 import { Loader } from "../app/Loader/Loader";
+import Modal from "@/app/Modal/Modal";
 
 type Props = {};
+type EmailData = {
+  Email: string;
+};
 
-const Dashboard = (props: Props) => {
+const Dashboard = (_props: Props) => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<EmailData[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [deletePopUp, setDeletePopUp] = useState(false);
+  const [deleteSuccessPopUp, setDeleteSuccessPopUp] = useState(false);
 
   const toggleSidebar = () => {
     setSidebarVisible(!sidebarVisible);
@@ -28,8 +34,12 @@ const Dashboard = (props: Props) => {
         const response = await axios.get(
           "https://waitlist-api-production-0759.up.railway.app/api/getWaitlist"
         );
-        setData(response.data);
-        console.log(response);
+        const sortedData = response.data.sort(
+          (a: { Timestamp: number }, b: { Timestamp: number }) =>
+            a.Timestamp - b.Timestamp
+        );
+        setData(sortedData);
+        // console.log(response);
         setTimeout(() => {
           setLoading(false);
         }, 2000);
@@ -38,10 +48,46 @@ const Dashboard = (props: Props) => {
         setLoading(false);
       }
     };
-
-    // Call the function to fetch data when the component mounts
     fetchData();
   }, []);
+
+  // const axiosInstance = axios.create({
+  //   baseURL: "https://waitlist-api-production-0759.up.railway.app/api",
+  //   timeout: 5000,
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+
+  // =====Function to delete an email=====
+  const deleteEmail = async (email: string) => {
+    try {
+      console.log("before");
+      const response = await axios.delete(
+        `https://waitlist-api-production-0759.up.railway.app/api/deleteWaitlist/${email}`
+      );
+      console.log(response, "here");
+
+      if (response.status === 200) {
+        // Close the delete confirmation pop-up
+        setDeletePopUp(false);
+        // Show the delete success pop-up
+        setDeleteSuccessPopUp(true);
+      }
+      const updatedData = data.filter(
+        (item: { Email: string }) => item.Email !== email
+      );
+      setData(updatedData);
+      console.log(updatedData);
+    } catch (error) {
+      alert("Unable to delete due to bad network");
+      console.error("Error deleting email:", error);
+    }
+  };
+
+  const handleDeleteConfirmation = (email: string) => {
+    deleteEmail(email);
+  };
 
   return (
     <div>
@@ -63,7 +109,7 @@ const Dashboard = (props: Props) => {
         <div className="flex justify-between">
           <h1 className="font-semibold md:text-[26px]">Waitlist Emails</h1>
           <Image
-            className="w-auto h-[20px] md:w-[35px] md:h-[35px] lg:w-[24px] lg:h-[24px]"
+            className="w-auto h md:w-[35px] md:h-[35px] lg:w-[24px] lg:h-[24px]"
             src="/droplist.png"
             alt="logout"
             width={20}
@@ -77,7 +123,7 @@ const Dashboard = (props: Props) => {
         </div>
         <div className="my-[6%] rounded-[5px] flex text-[14px] bg-[#EBEEFF] justify-between items-center p-2 md:p-4 lg:my-[2%]">
           <input
-            className="bg-transparent w-[100%] md:text-[20px]"
+            className="bg-transparent w-[100%] md:text-[20px] outline-none"
             type="text"
             placeholder="Search emails..."
             value={searchTerm}
@@ -107,13 +153,71 @@ const Dashboard = (props: Props) => {
                   key={index}
                 >
                   <p className="text-[#0008]">{item.Email}</p>
-                  <BiDotsVerticalRounded className="text-lg" />
+                  <BiDotsVerticalRounded
+                    // onClick={() => setDeletePopUp(true)}
+                    onClick={() => deleteEmail(item.Email)}
+                    className="text-lg"
+                  />
                 </li>
               ))}
             </ul>
           </div>
         )}
       </div>
+
+      {deletePopUp && (
+        <Modal>
+          <div className="w-[300px] h-[219px] shadow-[#0005] bg-white rounded-[11px]">
+            <hr className="mt-[10%] mb-[5%] h-[9px] bg-[#04177f] border-none md:h-[10px]" />
+            <p className=" text-[12px] font-semibold text-center">
+              Are you sure you want to delete this email ?
+            </p>
+
+            <div className="flex justify-center gap-[5%] mt-[25%]">
+              <button
+                // onClick={() => handleDeleteConfirmation(data[index].Email)}
+                // onClick={() => deleteEmail(item.Email)}
+                className="text-[10px] text-[#f95252] w-[95px] h-[22px] border-[1px] rounded-[7px]"
+              >
+                Yes
+              </button>
+              <button
+                onClick={() => setDeletePopUp(false)}
+                className="text-[10px] text-white w-[95px] h-[22px] bg-[#04177f] rounded-[7px]"
+              >
+                Exit
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
+
+      {deleteSuccessPopUp && (
+        <Modal>
+          <div className="w-[300px] h-[219px] shadow-[#0005] bg-white rounded-[11px]">
+            <hr className="mt-[10%] mb-[5%] h-[9px] bg-[#04177f] border-none md:h-[10px]" />
+            <p className=" text-[12px] font-semibold text-center">Successful</p>
+            <p className=" text-[12px] font-semibold text-center">
+              Email has been deleted successfully
+            </p>
+            <Image
+              className="mt-[2%] w-auto h-[50px] mx-auto lg:w-[60px] lg:h-[60px]"
+              src="/Gif/checkMarkGif.gif"
+              alt="/"
+              width={50}
+              height={50}
+            />
+            <div className="flex justify-center gap-[5%] mt-[12%]">
+              <button
+                onClick={() => setDeleteSuccessPopUp(false)}
+                className="text-[10px] text-white w-[95px] h-[22px] bg-[#04177f] rounded-[7px]"
+              >
+                Done
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
--- src/pages/Topbar.tsx
+++ src/pages/Topbar.tsx
@@ -1,18 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type Props = { toggleSideBar: () => void };
 
 export default function Topbar({ toggleSideBar }: Props) {
+  const [logout, setLogout] = useState(false);
+
   const handleMenuButtonClick = () => {
     toggleSideBar();
-  };
-
-  const router = useRouter();
-
-  const logout = () => {
-    router.push("/");
   };
 
   return (
@@ -27,7 +24,10 @@ export default function Topbar({ toggleSideBar }: Props) {
         onClick={handleMenuButtonClick}
       />
       <p className="text-[12px] md:text-[18px]">Admin Dashboard</p>
-      <div className="border-[2px] flex rounded-[8px]" onClick={logout}>
+      <div
+        className="border-[2px] flex rounded-[8px]"
+        onClick={() => setLogout(!logout)}
+      >
         {" "}
         <Image
           className=" h-[20px] md:w-[35px] md:h-[35px] lg:w-[24px] lg:h-[24px]"
@@ -44,6 +44,18 @@ export default function Topbar({ toggleSideBar }: Props) {
           height={20}
         />
       </div>
+      {logout && (
+        <ul className="z-[99px] bg-white border drop-shadow-xl absolute top-[5%] right-[5%]  ml-[12px] mt-[px] rounded-[3px] w-[95px] md:top-[95%] md:w-[114px] lg:rounded-[6px] lg:right-[4%] lg:top-[10.5%] lg:w-[200px] lg:ml-[19px]">
+          <li className="z-[99px] ursor-pointer hover:underline pt-1 pb-1 pl-1 text-[9px] font-medium border-b-[0.22px] md:border-b-[0.335px] md:text-[8px] lg:pt-[6%] lg:pb-[6%] lg:pl-[6%] lg:border-b-[0.6px] lg:text-[14px] ">
+            My Profile
+          </li>
+          <Link href="/">
+            <li className="z-[99px] cursor-pointer hover:underline pt-1 pb-1 pl-1 text-[9px] font-medium md:border-b-[0.335px] md:text-[8px] lg:pt-[6%] lg:pb-[6%] lg:pl-[6%] lg:border-b-[0.6px] lg:text-[14px] ">
+              Logout
+            </li>
+          </Link>
+        </ul>
+      )}
     </div>
   );
 }
